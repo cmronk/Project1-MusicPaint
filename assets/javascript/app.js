cb81642f85666a9d674dd2121aa688007efdefea
David T. Juarez Jr <djuarez1003@gmail.com>
2018-05-19 13:29:49 -0500

    added keyboard code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-console.log(two);
+console.log("two");
+SC.initialize({
+  client_id: 'YOUR_CLIENT_ID'
+});
+
+// stream track id 293
+SC.stream('/tracks/293').then(function(player){
+  player.play().then(function(){
+    console.log('Playback started!');
+  }).catch(function(e){
+    console.error('Playback rejected. Try calling play() from a user interaction.', e);
+  });
+});
