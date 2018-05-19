9665df156d93876d80c2585118a00ed10798ecb3
tzaher <tenillec@gmail.com>
2018-05-19 10:47:00 -0500

    adding Firebase code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
-console.log(two);
+
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDtY8RN8kHeOSCoIz3K6ZR5TFgarg2r-zA",
+    authDomain: "music-project-1.firebaseapp.com",
+    databaseURL: "https://music-project-1.firebaseio.com",
+    projectId: "music-project-1",
+    storageBucket: "music-project-1.appspot.com",
+    messagingSenderId: "881035365487"
+};
+firebase.initializeApp(config);
