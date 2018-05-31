84d40f8055cc501dbdbbb1cba55108f178069cd6
cronk <ChristineRonk@Stevens-MacBook-Pro.local>
2018-05-30 22:03:39 -0500

    pushing David's key changes and some styling.  Still working on playground and more styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 
-  // TZ Initialize Firebase
+  // only shows form initially until user specifies level
+$("#subwrapper").hide();
+
+
+  //Initialize Firebase
   var config = {
       apiKey: "AIzaSyDtY8RN8kHeOSCoIz3K6ZR5TFgarg2r-zA",
       authDomain: "music-project-1.firebaseapp.com",
@@ -12,18 +16,18 @@ $(document).ready(function () {
 
   var database = firebase.database();
 
-  // TZ Button for adding User
+  //Button for adding User
   $("#add-user-btn").on("click", function (event) {
       event.preventDefault();
 
-      // TZ Grabs user input
+      //Grabs user input
       var userName = $("#user-name-input").val().trim();
       var userLevel = $("#level-input").val().trim();
       var userSong = $("#song-input").val().trim();
       var userFav = $("#fav-input").val().trim();
 
 
-      // TZ Creates local "temporary" object for holding data
+      //Creates local "temporary" object for holding data
       var newUser = {
           name: userName,
           level: userLevel,
@@ -31,10 +35,10 @@ $(document).ready(function () {
           fav: userFav
       };
 
-      // TZ Uploads data to the database
+      //Uploads data to the database
       database.ref().push(newUser);
 
-      // TZ Clears all of the text-boxes
+      //Clears all of the text-boxes
       $("#user-name-input").val("");
       $("#level-input").val("");
       $("#song-input").val("");
@@ -42,27 +46,107 @@ $(document).ready(function () {
   });
 
 
-  // TZ Create Firebase event for adding data to the database 
+  //Create Firebase event for adding data to the database 
   database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
-      // TZ Store everything into a variable.
+      //Store everything into a variable.
       var userName = childSnapshot.val().name;
       var userLevel = childSnapshot.val().level;
       var userSong = childSnapshot.val().song;
       var userFav = childSnapshot.val().fav;
 
-      // TZ Add each data into the table
+      //Add each data into the table
       $("#music-table > tbody").append("<tr><td>" + userName + "</td><td>" + userLevel + "</td><td>" +
           userSong + "</td><td>" + userFav + "</td><td>");
   });
 
 
+// shows keyboard, youtube and user input
+$(document).on("click", ".btn", function(event) {
+  event.preventDefault();
+  $("#wrapper").hide();
+
+  $("#subwrapper").show();
 
 
 
 
-//Piano Click
-var synth = new Tone.Synth().toMaster();
+//Piano___________________________________
+var synth = new Tone.Synth().toMaster()
+var listItems = $(".dropdown-item");
+
+
+$("#ss1").click(function(){
+  synth = new Tone.AMSynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+$("#ss2").click(function(){
+  synth = new Tone.DuoSynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+$("#ss3").click(function(){
+  synth = new Tone.FMSynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+$("#ss4").click(function(){
+  synth = new Tone.MembraneSynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+// $("#ss5").click(function(){
+//   synth = new Tone.MetalSynth().toMaster()
+//   listItems.removeClass("active");
+//     $(this).toggleClass("active");
+//     console.log("5");
+//     console.log(synth);
+// })
+$("#ss6").click(function(){
+  synth = new Tone.MonoSynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+// $("#ss7").click(function(){
+//   synth = new Tone.MonophonicSynth().toMaster()
+//   listItems.removeClass("active");
+//     $(this).toggleClass("active");
+//     console.log("7");
+//     console.log(synth);
+// })
+// $("#ss8").click(function(){
+//   synth = new Tone.NoiseSynth().toMaster()
+//   listItems.removeClass("active");
+//     $(this).toggleClass("active");
+//     console.log("8");
+//     console.log(synth);
+// })
+$("#ss9").click(function(){
+  synth = new Tone.PluckSynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+$("#ss10").click(function(){
+  synth = new Tone.PolySynth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+$("#ss11").click(function(){
+  synth = new Tone.Synth().toMaster()
+  listItems.removeClass("active");
+    $(this).toggleClass("active");
+    console.log(synth);
+})
+
+//Piano Click________
 if (Tone.context.state !== 'running') {
   Tone.context.resume();
   // document.documentElement.addEventListener(
@@ -74,167 +158,155 @@ if (Tone.context.state !== 'running') {
 }
 $("body").on("click", "#c", function(){
   synth.triggerAttackRelease('C4', '8n')
-  Tone.context.resume();
-
-})
+});
+$("body").on("click", "#cSharp", function(){
+  synth.triggerAttackRelease('C#4', '8n')
+});
 $("body").on("click", "#d", function(){
   synth.triggerAttackRelease('D4', '8n')
-})
+});
+$("body").on("click", "#dSharp", function(){
+  synth.triggerAttackRelease('D#4', '8n')
+});
 $("body").on("click", "#e", function(){
   synth.triggerAttackRelease('E4', '8n')
-})
+});
 $("body").on("click", "#f", function(){
   synth.triggerAttackRelease('F4', '8n')
-})
+});
+$("body").on("click", "#fSharp", function(){
+  synth.triggerAttackRelease('F#4', '8n')
+});
 $("body").on("click", "#g", function(){
   synth.triggerAttackRelease('G4', '8n')
-})
+});
+$("body").on("click", "#gSharp", function(){
+  synth.triggerAttackRelease('G#4', '8n')
+});
 $("body").on("click", "#a", function(){
   synth.triggerAttackRelease('A4', '8n')
-})
+});
+$("body").on("click", "#aSharp", function(){
+  synth.triggerAttackRelease('A#4', '8n')
+});
 $("body").on("click", "#b", function(){
   synth.triggerAttackRelease('B4', '8n')
 });
+$("body").on("click", "#c1", function(){
+  synth.triggerAttackRelease('C5', '8n')
+});
+
 
 
 
 
 //______Key Events Test__________________________________________________________________________________________________
 
-//Keycode table
-var notes = {
-cNote : "synth.triggerAttack('C4')",
-cSNote : "synth.triggerAttack('C#4')",
-dNote : "synth.triggerAttack('D4')",
-dSNote : "synth.triggerAttack('D#4')",
-eNote : "synth.triggerAttack('E4')",
-fNote : "synth.triggerAttack('F4')",
-fSNote : "synth.triggerAttack('F#4')",
-gNote : "synth.triggerAttack('G4')",
-gSNote : "synth.triggerAttack('G#4')",
-aNote : "synth.triggerAttack('A4')",
-aSNote : "synth.triggerAttack('A#4')",
-bNote : "synth.triggerAttack('B4')",
-cHighNote : "synth.triggerAttack('C5')"
-}
-
-var pianoKeys = {
-  65 :  notes.cNote,
-  87 : notes.cSNote,
-  83 : notes.dNote,
-  69 : notes.dSNote,
-  68 : notes.eNote,
-  70 : notes.fNote,
-  84 : notes.fSNote,
-  71 : notes.gNote,
-  89 : notes.gSNote,
-  72 : notes.aNote,
-  85 : notes.aSNote,
-  74 : notes.bNote,
-  75 : notes.cHighNote,
-}
-
-var compKeys = {
-a : pianoKeys[65],
-w : pianoKeys[87],
-s : pianoKeys[83],
-e : pianoKeys[69],
-d : pianoKeys[68],
-f : pianoKeys[70],
-t : pianoKeys[84],
-g : pianoKeys[71],
-y : pianoKeys[89],
-h : pianoKeys[72],
-u : pianoKeys[85],
-j : pianoKeys[74],
-k : pianoKeys[75],
-}
 
 
-//____Stop repeat key on hold___________________________________________________
+
+//____Keydown events___________________________________________________
 var down = {};
-//Piano key C keyboard-A
 $(document).keydown(function(event){
+  Tone.context.resume();
+
      var keycode = (event.keyCode ? event.keyCode : event.which);
-     if(keycode == event.keyCode){
-          if (down[keycode] == null) { // first press
-            synth.triggerAttack('C4') 
-            $("#c").toggleClass("pressed");
-              down[keycode] = true; // record that the key's down
-          }
+     if(keycode == event.which){
+          if (down[event.which] == null) { // first press
+            var noteToPlay = ""
+            var keyToPress = ""
+            var blackKeyPress = ""
+            if(event.key.toLowerCase() == "a"){
+              noteToPlay = "C4";
+              keyToPress = $("#c");
+            } else if(event.key.toLowerCase() == "w"){
+              noteToPlay = "C#4";
+              blackKeyPress = $("#cSharp");
+            } else if(event.key.toLowerCase() == "s"){
+              noteToPlay = "D4";
+              keyToPress = $("#d");
+            } else if(event.key.toLowerCase() == "e"){
+              noteToPlay = "D#4";
+              blackKeyPress = $("#dSharp");
+            } else if(event.key.toLowerCase() == "d"){
+              noteToPlay = "E4";
+              keyToPress = $("#e");
+            } else if(event.key.toLowerCase() == "f"){
+              noteToPlay = "F4";
+              keyToPress = $("#f");
+            } else if(event.key.toLowerCase() == "t"){
+              noteToPlay = "F#4";
+              blackKeyPress = $("#fSharp");
+            } else if(event.key.toLowerCase() == "g"){
+              noteToPlay = "G4";
+              keyToPress = $("#g");
+            }else if(event.key.toLowerCase() == "y"){
+              noteToPlay = "G#4";
+              blackKeyPress = $("#gSharp");
+            }
+            else if(event.key.toLowerCase() == "h"){
+              noteToPlay = "A4";
+              keyToPress = $("#a");
+            }
+            else if(event.key.toLowerCase() == "u"){
+              noteToPlay = "A#4";
+              blackKeyPress = $("#aSharp");
+            }
+            else if(event.key.toLowerCase() == "j"){
+              noteToPlay = "B4";
+              keyToPress = $("#b");
+              
+            }else if(event.key.toLowerCase() == "k"){
+              noteToPlay = "C5";
+              keyToPress = $("#c1");
+            }else {}
+            if(noteToPlay){
+             synth.triggerAttack(noteToPlay);
+            } 
+            
+            if(keyToPress){
+              keyToPress.toggleClass("pressed");
+              keyToPress.toggleClass("bpressed");
+            }
+
+            if(blackKeyPress){
+              blackKeyPress.toggleClass("bpressed");
+            }
+           
+              down[event.which] = true; // record that the key's down
+            }
+          
+
+
+          
      } 
    $(this).keyup(function(event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
      down[keycode] = null;
-     synth.triggerRelease()
-    $("#c").removeClass("pressed");
-    $("#c").toggleClass("unpressed");
-   })
-});
-//_____Simple Piano Key Events_____________________________________________________________________________________________________-
-// //Piano key C keyboard-A
-// $(document).keydown(function(e) {
-//   if (e.repeat){return}
-//   if(e.which == 65) { 
-//     synth.triggerAttack('C4') 
-//     $("#c").toggleClass("pressed");
-//     }
+     if(keyToPress){
+      synth.triggerRelease()
+      keyToPress.removeClass("pressed");
+      keyToPress.toggleClass("unpressed");
+      } else if(blackKeyPress){
+        synth.triggerRelease()
+        blackKeyPress.removeClass("bpressed");
+        blackKeyPress.toggleClass("bunpressed");
+       
+      } else{}
+
+  
     
-//     $(this).keyup(function(){
-//       synth.triggerRelease()
-//     $("#c").removeClass("pressed");
-//     $("#c").toggleClass("unpressed");
-//     })
-// });
-// //Piano key C# keyboard-W
-// $(document).keydown(function(e) {
-//   if(e.which == 87) { 
-//     synth.triggerAttack('C#4') 
-//     $("#cSharp").toggleClass("bpressed");
-//     }
-//     $(this).keyup(function(){
-//       synth.triggerRelease()
-//     $("#cSharp").removeClass("bpressed");
-//     $("#cSharp").toggleClass("bunpressed");
-//     })
-// });
 
 
-// //Piano key D keyboard-S
-// $(document).keydown(function(e) {
-//   if(e.which == 83) { 
-//     synth.triggerAttack('D4') 
-//     $("#d").toggleClass("pressed");
-//     }
-// });
+   })
 
-// $(document).keyup(function(e) {
-//   if(e.which == 83) { 
-//     synth.triggerRelease()
-//     $("#d").removeClass("pressed");
-//     $("#d").toggleClass("unpressed");
-//     }
-// });
+});
 
-// //Piano key E keyboard-D
-// $(document).keydown(function(e) {
-//   if(e.which == 68) { 
-//     synth.triggerAttack('E4') 
-//     $("#e").toggleClass("pressed");
-//     }
-// });
+});
 
-// $(document).keyup(function(e) {
-//   if(e.which == 68) { 
-//     synth.triggerRelease() 
-//     $("#e").removeClass("pressed");
-//     $("#e").toggleClass("unpressed");
-//     }
-// });
+//____________________________________________________________
 
-
-
-
+// YOUTUBE API CALL
 
 $.ajax({
   url: "https://www.googleapis.com/youtube/v3/playlists?part=player&id=PLaq-_cPy3RZz-qNEAf7U1PMtJwJmxjrBT&key=AIzaSyAmH5A7wXlMygxeB0YdiFoHysWyoxtnMDo"
@@ -243,18 +315,14 @@ $.ajax({
 
   $("#player").append(data.items[0].player.embedHtml.replace("http://", "https://"))
 });
-// only shows form initially until user specifies level
-$("#subwrapper").hide();
 
-// shows keyboard, youtube and user input
-$(document).on("click", ".btn", function(event) {
-      event.preventDefault();
-      $("#wrapper").hide();
+$.ajax({
+  url: "https://www.googleapis.com/youtube/v3/playlists?part=player&search=" + search + "&key=AIzaSyAmH5A7wXlMygxeB0YdiFoHysWyoxtnMDo"
+}).then(function (data) {
+  console.log(data);
 
-      $("#subwrapper").show();
-
-  });
-
+  $("#player1").append(data.items[0].player.embedHtml.replace("http://", "https://"))
+});
 
 })
 
